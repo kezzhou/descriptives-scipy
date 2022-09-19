fde9502b7f479ab7ae23281a98f22a2fdac5e41f
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-09-19 00:02:15 -0400

    Update 3
    Added iris dataset and Berndt dataset (with seaborn example). Finished updating script and readme.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # descriptives-scipy
 HHA 507 // Week 4 // Assignment 4 // Part 1
+
+## this repo is a functional runthrough of scipy lecture found at: https://scipy-lectures.org/packages/statistics/index.html
+
+## all example data sets are provided in data folder. Script is found in script folder. script is mostly functional. errors occur at ANOVA testing section, where output specifies that the function is the wrong shape for coefficients, and the final section (interactions), in which statsmodels api ols module asks for endog and exdog parameters.
--- scripts/scipy.py
+++ scripts/scipy.py
@@ -12,6 +12,11 @@ import pandas.plotting as pdp ## the scipy lecture asks you to input 'from panda
 import numpy as np
 import researchpy as rp
 from scipy import stats
+import urllib
+import os
+import matplotlib.pyplot as plt
+import seaborn as sb
+import statsmodels.api as sm
 
 
 
@@ -29,7 +34,9 @@ data
 t = np.linspace(
     -6, 6, 20
     )
+
 sin_t = np.sin(t)
+
 cos_t = np.cos(t)
 
 pd.DataFrame(
@@ -174,7 +181,9 @@ stats.wilcoxon(
 ## test the difference between weights in males and females
 
 female_weight = data[data['Gender'] == 'Female']['Weight']
+
 male_weight = data[data['Gender'] == 'Male']['Weight']
+
 stats.ttest_ind(female_weight, male_weight)
 
 ## use non parametric statistics to test the difference between VIQ in males and females
@@ -182,3 +191,157 @@ stats.ttest_ind(female_weight, male_weight)
 stats.wilcoxon(
     data[data['Gender'] == 'Male']['VIQ'], data[data['Gender'] == 'Female']['VIQ']
 )
+
+
+
+
+#### Linear models, multiple factors, and analysis of variance ####
+
+## simple linear regression
+
+## for two sets of observations x and y, with y being the dependent, we want to test the hypothesis that y is a linear function of x
+
+## we are going to fit a linear model using ordinary least squares (OLS) with statsmodels
+
+x = np.linspace(-5, 5, 20)
+
+np.random.seed(1)
+
+## generating stimulated data according to model
+## normal distributed noise
+
+y = -5 + 3*x + 4 * np.random.normal(size=x.shape)
+
+## create a df containing all the relevant variables
+
+data = pd.DataFrame({'x': x, 'y': y})
+
+from statsmodels.formula.api import ols
+
+model = ols("y ~ x", data).fit()
+
+print(model.summary()) ## let's inspect the stats from the ols fit
+
+
+## categorical variables
+
+data = pd.read_csv('data/brain_size.csv', sep=';', na_values=".") ## we will reuse the example data
+
+model = ols("VIQ ~ Gender + 1", data).fit() 
+
+print(model.summary()) ## let's compare IQ in males and females as a linear model and view it 
+
+model = ols('VIQ ~ C(Gender)', data).fit() ## if we want to force a numerical variable to be treated as a categorical variable, we can use this
+## in this way, a numerical column like VIQ can be treated the same as a categorical one like Gender
+
+## Now we want to compare IQ types by creating a long-form table by listing IQs and their type
+
+data_fisq = pd.DataFrame({'iq': data['FSIQ'], 'type': 'fsiq'})
+
+data_piq = pd.DataFrame({'iq': data['PIQ'], 'type': 'piq'})
+
+data_long = pd.concat((data_fisq, data_piq))
+
+print(data_long)
+
+stats.ttest_ind(data['FSIQ'], data['PIQ']) ## let's double check our results by comparing the p values we got for the earlier t test
+
+
+## multiple regression
+
+## this time we consider a model using z as the dependent variable and x and y as the independents
+## we'll use dataset iris.csv
+
+data = pd.read_csv('examples/iris.csv')
+
+model = ols('sepal_width ~ name + petal_length', data).fit()
+
+print(model.summary())
+
+## ANOVA testing
+
+print(model.f_test([0, 1, -1, 0]))
+
+
+
+
+#### Seaborn! ####
+
+if not os.path.exists('wages.txt'):
+
+    ## Download the example data file
+
+    urllib.request.urlretrieve('http://lib.stat.cmu.edu/datasets/CPS_85_Wages', ## scipy lecture asks you to run .urlretrieve, we run .request.urlretrieve here
+                       'data/wages.txt')
+
+## Give names to the columns
+
+names = [
+    'EDUCATION: Number of years of education',
+    'SOUTH: 1=Person lives in South, 0=Person lives elsewhere',
+    'SEX: 1=Female, 0=Male',
+    'EXPERIENCE: Number of years of work experience',
+    'UNION: 1=Union member, 0=Not union member',
+    'WAGE: Wage (dollars per hour)',
+    'AGE: years',
+    'RACE: 1=Other, 2=Hispanic, 3=White',
+    'OCCUPATION: 1=Management, 2=Sales, 3=Clerical, 4=Service, 5=Professional, 6=Other',
+    'SECTOR: 0=Other, 1=Manufacturing, 2=Construction',
+    'MARR: 0=Unmarried,  1=Married',
+]
+
+short_names = [n.split(':')[0] for n in names]
+
+data = pd.read_csv('data/wages.txt', skiprows=27, skipfooter=6, sep=None,
+                       header=None)
+
+data.columns = short_names
+
+## Log-transform the wages, because they typically are increased with multiplicative factors
+
+data['WAGE'] = np.log10(data['WAGE'])
+
+## pairplot: scatter matrices
+
+sb.pairplot(data, vars=['WAGE', 'AGE', 'EDUCATION'],
+                      kind='reg')
+
+sb.pairplot(data, vars=['WAGE', 'AGE', 'EDUCATION'],
+                      kind='reg', hue='SEX')
+plt.suptitle('Effect of gender: 1=Female, 0=Male')
+
+sb.pairplot(data, vars=['WAGE', 'AGE', 'EDUCATION'],
+                      kind='reg', hue='RACE')
+plt.suptitle('Effect of race: 1=Other, 2=Hispanic, 3=White')
+
+sb.pairplot(data, vars=['WAGE', 'AGE', 'EDUCATION'],
+                        kind='reg', hue='UNION')
+
+plt.suptitle('Effect of union: 1=Union member, 0=Not union member')
+
+## simple regression plot 
+## a regression capturing the relation between one variable and another, eg wage and eduction, can be plotted using sb.lmplot():
+
+sb.lmplot(y='WAGE', x='EDUCATION', data=data)
+
+plt.show() ## view data on multiple plots
+
+print(data) ## view data in traditional table format
+
+from matplotlib import pyplot as plt
+plt.rcdefaults() ## if for some reason a user prefers trad matplotlib styling instead of more modern seaborn styling, this code will do the trick on the version revert
+
+
+## to use a robust model, which is less sensitive to outliers, we can specify robust=True in seaborn plotting functions
+## or we can replace OLS with robust linear model (statsmodels.formula.api.rlm()) in statsmodels
+
+
+
+
+#### Testing for interactions #### 
+
+## we have plotted various different fits. if we now want to formulate a single model to test for variance across slopes between two populations, we use interactions
+
+result = sm.OLS(formula='wage ~ education + gender + education * gender', data=data).fit() ## OLS asks for parameters endog and exdog
+
+print(result.summary())
